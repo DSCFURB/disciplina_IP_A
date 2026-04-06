57ff5c930a2302307793471570315bb666a62dc5
lucianapda <lpa@furb.br>
2026-04-06 09:04:25 -0300

    Classe do desafio beecrownd
    Lembre-se de alterar o nome da classe para Main.java

---
+++ Unidade4/src/desafio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        //****Código inicial****
+
+        int quantidadeVezes = s.nextInt();
+        for (int i = 0 ; i < quantidadeVezes; i++) {
+            //*** lógica para identificação de cada uma das entradas ****
+            //PARTE QUE SE REPETE
+            
+        }
+
+        s.close();
+    }
+}
